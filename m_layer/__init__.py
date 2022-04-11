a2c0d246e2ebc4d13f7ed8d46151f116f96f42f1
B D Hall <36650103+BlairDHall@users.noreply.github.com>
2022-04-12 10:13:53 +1200

    Update __init__.py
    Version changed from dev0

--- m_layer/__init__.py
+++ m_layer/__init__.py
@@ -1,3 +1,3 @@
 #----------------------------------------------------------------------------
-version = "0.1.0.dev0"
+version = "0.1.0"
 copyright = """Copyright (c) 2022, Asia-Pacific Metrology Programme Focus Group on Digital Transformation in Metrology (APMP-DXFG)"""
