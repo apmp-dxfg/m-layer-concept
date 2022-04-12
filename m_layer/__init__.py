4aba33fde8403908632bf1b66f4a3377c951c379
Blair Hall <blairdhall@gmail.com>
2022-04-12 15:05:00 +1200

    Modified __init__.py to support usage: from m_layer import *
    The default context is now exported with the AspectValue class
    and convert function.

--- m_layer/__init__.py
+++ m_layer/__init__.py
@@ -1,3 +1,13 @@
+from m_layer.context import default_context
+from m_layer.aspect_value import (AspectValue, AV, convert)
+
+__all__ = (
+    'AspectValue', 'AV', 'convert',
+    'default_context',    
+)
+
 #----------------------------------------------------------------------------
-version = "0.1.0"
+version = "0.2.0dev0"
 copyright = """Copyright (c) 2022, Asia-Pacific Metrology Programme Focus Group on Digital Transformation in Metrology (APMP-DXFG)"""
+
+
