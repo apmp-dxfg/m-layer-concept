1aff461782d6636745fd441ec86744d97c111de9
Blair Hall <blairdhall@gmail.com>
2022-04-13 15:38:43 +1200

    These files provide the grouping between aspects and legitimate extended scales

---
+++ m_layer/scales_for_aspect_register.py
@@ -0,0 +1,24 @@
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+
+from m_layer.register import Register
+
+# ---------------------------------------------------------------------------
+class ScalesForAspectRegister(Register):
+    """
+    """
+    
+    def __init__(self,context):
+        super().__init__(context) 
+        
+    def set(self,entry):
+        uid = tuple( entry['aspect'] )
+        
+        if uid in self._objects:
+            raise RuntimeError(
+                "existing register entry: {}".format(uid)
+            )
+        else:
+            self._objects[uid] = [
+                tuple( json_id ) 
+                    for json_id in entry['scales']
+            ]
