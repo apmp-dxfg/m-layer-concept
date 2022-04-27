d580209c8cfaf451ead0c800d32221372b6ac4d4
Blair Hall <blairdhall@gmail.com>
2022-04-27 16:31:23 +1200

    Adapted to changes

--- examples/example_mass.py
+++ examples/example_mass.py
@@ -1,9 +1,5 @@
 from m_layer import *
 
-from m_layer import aspect
-from m_layer import si_unit 
-from m_layer import imperial_unit 
-
 def display(av):
     print(av)
     print(repr(av))
@@ -12,6 +8,6 @@ def display(av):
 x = AV(aspect.mass,12,si_unit.kg)
 display(x)
 
-y = convert(x,imperial_unit.pound)
+y = x.convert(imperial_unit.pound)
 display(y)
 
