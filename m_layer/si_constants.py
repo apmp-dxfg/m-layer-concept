6ac35a7499ab8e58a132b5cc5ce3497c74e43795
Blair Hall <blairdhall@gmail.com>
2022-06-14 11:58:47 +1200

    Holds SI constants

---
+++ m_layer/si_constants.py
@@ -0,0 +1,9 @@
+"""
+Numerical values for some of the physical constants given 
+explicit exact values in the 9th edition of the SI Brochure. 
+"""
+e = 1.602176634E-19     # coulomb
+c = 2.99792458E8        # meter per second
+h = 6.62607015E-34      # joule second
+k = 1.380649E-23        # joule per kelvin
+
