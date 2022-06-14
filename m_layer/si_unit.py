586c761292588f9aa2dd7942ce897ef2f7da71ad
Blair Hall <blairdhall@gmail.com>
2022-06-14 15:18:55 +1200

    Deleted PY interface files

--- m_layer/si_unit.py
+++
@@ -1,7 +0,0 @@
-kg = tuple( ["ml-si-kilogram-ratio",12782167041499057092439851237297548539] )
-rad = tuple( ["ml-si-radian-ratio", 273301153578020696303516833405033923738] )
-rad_pi = tuple( ["ml-si-radian-bounded-pi", 181367268705518406168243034119604185497] )
-rad_2pi = tuple( ["ml-si-radian-bounded-two-pi", 300556212736422769570885306883285535638] )
-deg_C_interval = tuple( ["ml-si-celsius-interval", 245795086332095731716589481707012001072] )
-deg_C_ratio = tuple( ["ml-si-celsius-ratio", 278784445377172064355281533676474538407] )
-kelvin = tuple( ["ml-si-kelvin-ratio", 302952256288207449238881076502466548054] )
