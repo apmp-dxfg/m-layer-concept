f75cd92a4adef5edc234ad2e7af302f3807050df
Blair Hall <blairdhall@gmail.com>
2022-06-15 15:50:51 +1200

    Updated test

--- tests/test_init.py
+++ tests/test_init.py
@@ -2,15 +2,17 @@ import unittest
 
 from m_layer import * 
 
-from m_layer import aspect
-from m_layer import si_unit 
+ml_mass = ('ml-mass', 321881801928222308627062904049725548287)
+
+ml_si_kilogram_ratio = ('ml-si-kilogram-ratio', 12782167041499057092439851237297548539)
+ml_imp_pound_ratio = ('ml-imp-pound-ratio', 188380796861507506602975683857494523991)
 
 #----------------------------------------------------------------------------
 class TestInit(unittest.TestCase):
 
     def test(self):
         # construction
-        x = AV(aspect.mass,12,si_unit.kg)
+        x = XP(ml_mass,12,ml_si_kilogram_ratio)
             
 #============================================================================
 if __name__ == '__main__':
