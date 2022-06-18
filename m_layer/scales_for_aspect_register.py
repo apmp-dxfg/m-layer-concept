b8a2cd39cae3688697008ab6873f5de35959414f
Blair Hall <blairdhall@gmail.com>
2022-06-18 17:26:27 +1200

    The scales_for_aspect_register is now used for cases where we explicitly need to allow conversion, because the scales involved are not normally equivalent.

--- m_layer/scales_for_aspect_register.py
+++ m_layer/scales_for_aspect_register.py
@@ -1,27 +1,42 @@
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+import json
+
 """
 Declared conversions must be enabled for each aspect.
 This register holds a sequence of M-layer scales for each aspect. 
 """
-from m_layer.register import Register
+# Numerical terms in JSON strings are converted to numbers using `eval()`. 
+import math
+from m_layer import si_constants as si 
+
+from m_layer.conversion_register import _set_conversion_fn
 
 # ---------------------------------------------------------------------------
-class ScalesForAspectRegister(Register):
+class ScalesForAspectRegister(object):
     """
     """
     
     def __init__(self,context):
-        super().__init__(context) 
+        self._context = context 
+        # Table indexed by aspect to entries that are 
+        # mapped by scale-pairs to a conversion function
+        # (same format as conversion_register entries)
+        
+        self._table = {}
+                
         
     def set(self,entry):
+        # keys: aspect, src, dst, factors
         uid = tuple( entry['aspect'] )
+
+        if uid not in self._table:
+            self._table[uid] = {}
+                    
+        uid_ml_ref_src = tuple( entry['src'] )        
+        uid_ml_ref_dst = tuple( entry['dst'] )
+            
+        uid_pair = (uid_ml_ref_src,uid_ml_ref_dst)
+        _set_conversion_fn(self,entry,self._table[uid],uid_pair)
+       
+            
         
-        if uid in self._objects:
-            raise RuntimeError(
-                "existing register entry: {}".format(uid)
-            )
-        else:
-            self._objects[uid] = [
-                tuple( json_id ) 
-                    for json_id in entry['scales']
-            ]
