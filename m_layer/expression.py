2fb63c45d4f651dd62fa96ba398f09552e3ec9a4
Blair Hall <blairdhall@gmail.com>
2022-06-18 17:27:16 +1200

    The way that conversions are allowed as changed

--- m_layer/expression.py
+++ m_layer/expression.py
@@ -16,6 +16,7 @@ class Expression(object):
     
     # Notation distinguishes between an M-Layer reference ('ml_ref'), which 
     # includes information about the scale type, and a conventional reference.
+    
     __slots__ = ("_value","_ml_ref","_aspect")
     
     def __init__(self,aspect,value,ref):
@@ -77,18 +78,36 @@ class Expression(object):
         Conversion does not change the aspect or the type of scale
         
         """
-        if ml_ref in cxt.scales_for_aspect_reg[self._aspect]:
-            
-            fn = cxt.conversion_fn(self,ml_ref)
+        
+        # If there is a src-dst pair of scales in the register
+        # then use it, without checking aspect.
+        fn = cxt.conversion_reg.get( (self._ml_ref,ml_ref) ) 
+        if fn is not None:
+        # if ( 
+            # (self._ml_ref,ml_ref) in cxt.conversion_reg
+        # or
+            # ml_ref in cxt.scales_for_aspect_reg[self._aspect]
+        # ):         
+            # fn = cxt.conversion_fn(self,ml_ref)
             return Expression(
                 self._aspect,
                 fn( self._value ),
                 ml_ref
             )
-        else: 
-            raise RuntimeError(
-                "cannot convert {!r} to {!r}".format(self._ml_ref,ml_ref)
+            
+        # If the first search fails, look for additional 
+        # conversions that are aspect specific 
+        # (e.g., wavenumber to frequency for photon energy)
+        else:
+            pass
+            
+        # This is a failure    
+        raise RuntimeError(
+            "no conversion for '{!r}' to '{!r}'".format(
+                self,
+                ml_ref
             )
+        )
 
     # ---------------------------------------------------------------------------
     def cast(self,aspect_dst,scale_dst):
