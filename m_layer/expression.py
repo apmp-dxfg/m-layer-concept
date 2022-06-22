31a9727bd779569aa0b2622a7da849f2e16ebacc
Blair Hall <blairdhall@gmail.com>
2022-06-22 15:27:36 +1200

    Aspects will follow scales (ordering of arguments etc) This allows aspect to assume default values

--- m_layer/expression.py
+++ m_layer/expression.py
@@ -6,23 +6,19 @@ cxt = context.default_context
 __all__ = (
     'Expression', 'XP'
 )
-
   
 # ---------------------------------------------------------------------------
 class Expression(object):
     
     """
-    An `Expression` is defined by an aspect, a value and a scale. 
+    An `Expression` is defined by a value, a scale and an aspect. 
     """
     
-    # Notation distinguishes between an M-Layer reference ('ml_ref'), which 
-    # includes information about the scale type, and a conventional reference.
+    __slots__ = ("_value","_scale","_aspect")
     
-    __slots__ = ("_value","_ml_ref","_aspect")
-    
-    def __init__(self,value,ref,aspect=None):
+    def __init__(self,value,scale,aspect=None):
         self._value = value 
-        self._ml_ref = ref
+        self._scale = scale
         self._aspect = aspect
         
     def __str__(self):
@@ -53,7 +49,7 @@ class Expression(object):
         Return the unit (or reference) as a string 
         
         """
-        scale_json = cxt.scale_reg[self._ml_ref] 
+        scale_json = cxt.scale_reg[self._scale] 
         ref_uid = cxt.reference_reg[ tuple(scale_json['reference']) ] 
         
         locale_key = 'symbol' if short else 'name'
@@ -66,70 +62,55 @@ class Expression(object):
         Return the aspect as a string 
         
         """
-        aspect_json = cxt.aspect_reg[self._aspect] 
+        if self._aspect is not None:
+            aspect_json = cxt.aspect_reg[self._aspect] 
 
-        locale_key = 'symbol' if short else 'name'
-        if locale is None: locale = cxt.locale 
-            
-        return aspect_json['locale'][locale][locale_key]
+            locale_key = 'symbol' if short else 'name'
+            if locale is None: locale = cxt.locale 
+                
+            return aspect_json['locale'][locale][locale_key]
+        else:
+            return ""
 
 
     # ---------------------------------------------------------------------------
-    def convert(self,ml_ref):
+    def convert(self,dst_scale):
         """
         Return a new `Expression` in terms of the scale provided
         
         """
-        
-        # If there is a pair of scales in the register
-        # then use it without checking aspect.
-        fn = cxt.conversion_reg.get( (self._ml_ref,ml_ref) ) 
-        
-        if fn is not None:
-        # if ( 
-            # (self._ml_ref,ml_ref) in cxt.conversion_reg
-        # or
-            # ml_ref in cxt.scales_for_aspect_reg[self._aspect]
-        # ):         
-            # fn = cxt.conversion_fn(self,ml_ref)
-            return Expression(
-                fn( self._value ),
-                ml_ref,
-                self._aspect
-            )
-            
-        # If the first search fails, look for additional 
-        # conversions that are aspect specific 
-        # (e.g., wavenumber to frequency for photon energy)
-        # Need to check that aspect != None
-        else:
-            assert False
-            
-        # This is a failure    
-        raise RuntimeError(
-            "no conversion for '{!r}' to '{!r}'".format(
-                self,
-                ml_ref
-            )
+        fn = cxt.conversion_fn(self,dst_scale) 
+ 
+        return Expression(
+            fn( self._value ),
+            dst_scale,
+            self._aspect
         )
 
     # ---------------------------------------------------------------------------
-    def cast(self,aspect_dst,scale_dst):
+    def cast(self,dst_scale,dst_aspect=None):
         """
         Return a new `Expression` in terms of the aspect and scale provided
-        
+
+        If the argument `dst_aspect` is not specified it
+        will default to be the same aspect as `self`.
+                
         """
         if self._aspect is None:
             raise RuntimeError(
                 "{!r} has no declared aspect, so it cannot be cast".format(self)
             )
+
+        # No destination aspect => reuse the object's aspect
+        if dst_aspect is None: 
+            dst_aspect = self._aspect
         
-        fn = cxt.casting_fn(self, (aspect_dst,scale_dst) )
+        fn = cxt.casting_fn(self, (dst_scale, dst_aspect) )
         
         return Expression(
             fn( self._value ),
-            scale_dst,
-            aspect_dst
+            dst_scale,
+            dst_aspect
         )
 
 XP = Expression
