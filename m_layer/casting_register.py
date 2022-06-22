12e2526f439ded02c9775b4d457fb2d3dc5633ad
Blair Hall <blairdhall@gmail.com>
2022-06-22 15:27:06 +1200

    Aspects will follow scales (ordering of arguments etc)

--- m_layer/casting_register.py
+++ m_layer/casting_register.py
@@ -3,6 +3,7 @@ import json
 
 # Terms in JSON strings for numerical factors are converted to numbers using 
 # `eval()`. By importing math, we can include defined constants, like math.pi.
+
 import math
 
 from m_layer import si_constants as si 
@@ -20,7 +21,7 @@ def to_tuple(lst):
 class CastingRegister(object):
     
     """
-    Holds a mapping of aspect-scale pairs 
+    Holds a mapping of scale-aspect pairs 
     to information about how to cast between expressions. 
     """
     
@@ -38,10 +39,9 @@ class CastingRegister(object):
         """
         Return a conversion function 
         """
-        # `uid` may be a list from json
         return self._table.get( uid_pair, default ) 
         
-    # Extract an aspect-scale pair for the entry and then a function
+    # Extract an scale-aspect pair for the entry and then a function
     # to cast from one value to the another for the expression.
     def set(self,entry):
         """
