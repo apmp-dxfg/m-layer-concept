ef7fff7537e9b5c8023216724c542736c5338fa5
Blair Hall <blairdhall@gmail.com>
2022-06-22 15:28:24 +1200

    Filled out the behaviour of the ScalesForAspectRegister

--- m_layer/scales_for_aspect_register.py
+++ m_layer/scales_for_aspect_register.py
@@ -2,13 +2,11 @@
 import json
 
 """
-Declared conversions must be enabled for each aspect.
-This register holds a sequence of M-layer scales for each aspect. 
 """
 # Numerical terms in JSON strings are converted to numbers using `eval()`. 
 import math
-from m_layer import si_constants as si 
 
+from m_layer import si_constants as si 
 from m_layer.conversion_register import _set_conversion_fn
 
 # ---------------------------------------------------------------------------
@@ -18,25 +16,42 @@ class ScalesForAspectRegister(object):
     
     def __init__(self,context):
         self._context = context 
-        # Table indexed by aspect to entries that are 
-        # mapped by scale-pairs to a conversion function
+        
+        # Table indexed by aspect with entries that are 
+        # mapping scale-pairs to conversion functions
         # (same format as conversion_register entries)
         
         self._table = {}
+ 
+    # These mapping methods just act on the aspect table
+    def __contains__(self,aspect):
+        return aspect in self._table 
+
+    def __getitem__(self,aspect):
+        return self._table[ aspect ]
+ 
+    def get(self,aspect,default):
+        return self._table.get( aspect, default )
+
+    # This method goes down to the second level 
+    def get_fn(self,aspect,scale_uid_pair,default=None):
+        """
+        Return a conversion function 
+        """        
+        return self._table.get( aspect, {} ).get( scale_uid_pair, default ) 
                 
-        
     def set(self,entry):
         # keys: aspect, src, dst, factors
-        uid = tuple( entry['aspect'] )
+        uid_aspect = tuple( entry['aspect'] )
 
-        if uid not in self._table:
-            self._table[uid] = {}
+        if uid_aspect not in self._table:
+            self._table[uid_aspect] = {}
                     
         uid_ml_ref_src = tuple( entry['src'] )        
         uid_ml_ref_dst = tuple( entry['dst'] )
             
-        uid_pair = (uid_ml_ref_src,uid_ml_ref_dst)
-        _set_conversion_fn(self,entry,self._table[uid],uid_pair)
+        scale_uid_pair = (uid_ml_ref_src,uid_ml_ref_dst)
+        _set_conversion_fn(self,entry,self._table[uid_aspect],scale_uid_pair)
        
             
         
