0e37ba2a9c1bfb97eb18b23fb6a6c9b6f277167a
Blair Hall <blairdhall@gmail.com>
2022-06-28 12:01:30 +1200

    Ouput from m_layer/ml_to_py_key.py now produces Aspect or Scale declarations

--- m_layer/ml_to_py_key.py
+++ m_layer/ml_to_py_key.py
@@ -8,11 +8,15 @@ A sequence of JSON file names can be given on the command line.
 """
 import json 
 import sys 
+import glob 
 import os.path
 
 # set as appropriate 
-root_dir = r"C:\proj_py\m-layer-concept\m_layer\json\scales"
-# root_dir = r"C:\proj_py\m-layer-concept\m_layer\json\aspects"
+# root_dir = r"C:\proj_py\m-layer-concept\m_layer\json\scales"
+# fmt = r"{} = Scale( ('{}', {}) )"
+
+root_dir = r"C:\proj_py\m-layer-concept\m_layer\json\aspects"
+fmt = r"{} = Aspect( ('{}', {}) )"
 
 # One or more JSON file names (without extension)
 if len(sys.argv) > 1:
@@ -30,7 +34,24 @@ if len(sys.argv) > 1:
             
             d = d_i['uid']
             new_name = d[0].replace('-','_')
-            str_exp = "{} = ('{}', {})".format(new_name,d[0],d[1])
+            str_exp = fmt.format(new_name,d[0],d[1])
+        
+            print(str_exp)
+            
+        print()
+else:
+    file_path = os.path.join( root_dir,'*.json' )
+    
+    for f_json in glob.glob( file_path ):
+        print(f_json)
+        with open(f_json,'r') as f:
+            data = json.load(f)        
+        
+        for d_i in data:
+            
+            d = d_i['uid']
+            new_name = d[0].replace('-','_')
+            str_exp = fmt.format(new_name,d[0],d[1])
         
             print(str_exp)
             
