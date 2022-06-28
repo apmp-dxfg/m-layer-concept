29a7e359edf8892712f31803bcffa4ce1e760bad
Blair Hall <blairdhall@gmail.com>
2022-06-28 12:00:07 +1200

    Examples all now used the classes Aspect and Scale

--- examples/example_mass.py
+++ examples/example_mass.py
@@ -5,10 +5,10 @@ def display(xp):
     print(repr(xp))
     print()
 
-ml_mass = ('ml-mass', 321881801928222308627062904049725548287)
+ml_mass = Aspect( ('ml-mass', 321881801928222308627062904049725548287) )
 
-ml_si_kilogram_ratio = ('ml-si-kilogram-ratio', 12782167041499057092439851237297548539)
-ml_imp_pound_ratio = ('ml-imp-pound-ratio', 188380796861507506602975683857494523991)
+ml_si_kilogram_ratio = Scale( ('ml-si-kilogram-ratio', 12782167041499057092439851237297548539) )
+ml_imp_pound_ratio = Scale( ('ml-imp-pound-ratio', 188380796861507506602975683857494523991) )
 
 # x = XP(12,ml_si_kilogram_ratio,ml_mass)
 x = XP(12,ml_si_kilogram_ratio)
--- examples/example_temperature.py
+++ examples/example_temperature.py
@@ -1,10 +1,10 @@
 from m_layer import *
 
-ml_temperature = ('ml-temperature', 316901515895475271730171605211001099255)
+ml_temperature = Aspect( ('ml-temperature', 316901515895475271730171605211001099255) )
 
-ml_si_kelvin_ratio = ('ml-si-kelvin-ratio', 302952256288207449238881076502466548054)
-ml_si_celsius_interval = ('ml-si-celsius-interval', 245795086332095731716589481707012001072)
-ml_imp_fahrenheit_interval = ('ml-imp-fahrenheit-interval', 22817745368296240233220712518826840767)
+ml_si_kelvin_ratio = Scale( ('ml-si-kelvin-ratio', 302952256288207449238881076502466548054) )
+ml_si_celsius_interval = Scale( ('ml-si-celsius-interval', 245795086332095731716589481707012001072) )
+ml_imp_fahrenheit_interval = Scale( ('ml-imp-fahrenheit-interval', 22817745368296240233220712518826840767) )
 
 def display(xp):
     print(xp)
