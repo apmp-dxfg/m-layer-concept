8d620dcf21f94d0f70ce035f36c3e21f074b7726
Blair Hall <blairdhall@gmail.com>
2022-06-30 15:06:56 +1200

    si prefixes

--- m_layer/si_constants.py
+++ m_layer/si_constants.py
@@ -7,3 +7,21 @@ c = 2.99792458E8        # meter per second
 h = 6.62607015E-34      # joule second
 k = 1.380649E-23        # joule per kelvin
 
+atto    =   1E-18
+femto   =   1E-15
+pico    =   1E-12
+nano    =   1E-9
+micro   =   1E-6
+milli   =   1E-3
+centi   =   1E-2
+deci    =   1E-1
+deca    =   1E1
+hecto   =   1E2  
+kilo    =   1E3 
+mega    =   1E6 
+giga    =   1E9
+tera    =   1E12
+peta    =   1E15
+
+
+
