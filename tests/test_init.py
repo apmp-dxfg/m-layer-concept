41f190f2e3670b47203e186b6def5d4676e383d8
Blair Hall <blairdhall@gmail.com>
2022-07-03 17:30:19 +1200

    Updated test to API changes

--- tests/test_init.py
+++ tests/test_init.py
@@ -2,17 +2,17 @@ import unittest
 
 from m_layer import * 
 
-ml_mass = ('ml-mass', 321881801928222308627062904049725548287)
+ml_mass = Aspect( ('ml-mass', 321881801928222308627062904049725548287) )
 
-ml_si_kilogram_ratio = ('ml-si-kilogram-ratio', 12782167041499057092439851237297548539)
-ml_imp_pound_ratio = ('ml-imp-pound-ratio', 188380796861507506602975683857494523991)
+ml_si_kilogram_ratio = Scale( ('ml-si-kilogram-ratio', 12782167041499057092439851237297548539) )
+ml_imp_pound_ratio = Scale( ('ml-imp-pound-ratio', 188380796861507506602975683857494523991) )
 
 #----------------------------------------------------------------------------
 class TestInit(unittest.TestCase):
 
     def test(self):
         # construction
-        x = XP(ml_mass,12,ml_si_kilogram_ratio)
+        x = expr(12,ml_si_kilogram_ratio,ml_mass)
             
 #============================================================================
 if __name__ == '__main__':
