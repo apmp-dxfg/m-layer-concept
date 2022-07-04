98004235a05e5fa1add0650ec9b5a11468ac1c5e
Blair Hall <blairdhall@gmail.com>
2022-07-04 16:38:29 +1200

    Working with examples for the docs now

--- examples/example_mass.py
+++ examples/example_mass.py
@@ -10,10 +10,12 @@ ml_mass = Aspect( ('ml-mass', 321881801928222308627062904049725548287) )
 ml_si_kilogram_ratio = Scale( ('ml-si-kilogram-ratio', 12782167041499057092439851237297548539) )
 ml_imp_pound_ratio = Scale( ('ml-imp-pound-ratio', 188380796861507506602975683857494523991) )
 
-# x = XP(12,ml_si_kilogram_ratio,ml_mass)
-x = expr(12,ml_si_kilogram_ratio)
+x = expr(12,ml_si_kilogram_ratio,ml_mass)
 display(x)
 
-y = x.convert(ml_imp_pound_ratio)
+display(x.convert(ml_imp_pound_ratio))
+
+y = expr(12,ml_si_kilogram_ratio)
 display(y)
 
+display(y.convert(ml_imp_pound_ratio))
--- examples/example_temperature.py
+++ examples/example_temperature.py
@@ -1,23 +1,29 @@
 from m_layer import *
 
-ml_temperature = Aspect( ('ml-temperature', 316901515895475271730171605211001099255) )
-
-ml_si_kelvin_ratio = Scale( ('ml-si-kelvin-ratio', 302952256288207449238881076502466548054) )
-ml_si_celsius_interval = Scale( ('ml-si-celsius-interval', 245795086332095731716589481707012001072) )
-ml_imp_fahrenheit_interval = Scale( ('ml-imp-fahrenheit-interval', 22817745368296240233220712518826840767) )
-
 def display(xp):
     print(xp)
     print(repr(xp))
     print()
 
-t = expr(72, ScaleAspect( ml_imp_fahrenheit_interval,ml_temperature) )
-# t = XP(72,ml_imp_fahrenheit_interval)
+T = Aspect( ('ml-temperature', 316901515895475271730171605211001099255) )
+dT = Aspect( ('ml-temperature-difference', 212368324110263031011700652725345220325) )
+ITS_90 = Aspect( ('ml-ITS-90', 333567868916523888067500483865254994090) )
+
+kelvin = Scale( ('ml-si-kelvin-ratio', 302952256288207449238881076502466548054) )
+celsius_ratio = Scale( ('ml-si-celsius-ratio', 278784445377172064355281533676474538407) )
+celsius_interval = Scale( ('ml-si-celsius-interval', 245795086332095731716589481707012001072) )
+fahrenheit = Scale( ('ml-imp-fahrenheit-interval', 22817745368296240233220712518826840767) )
+
+t = expr(72,fahrenheit)
 display(t)
 
-t_C = t.convert(ml_si_celsius_interval)
+t_C = t.convert(celsius_interval)
 display(t_C)
 
 # t_K = t_C.cast(ml_si_kelvin_ratio,ml_temperature)
-t_K = t_C.cast(ml_si_kelvin_ratio)  # unspecified => same aspect
-display(t_K)
+# t_K = cast(t_C,kelvin)  # unspecified => same aspect
+# display(t_K)
+
+t_diff_C = expr(10,celsius_ratio)
+display(t_diff_C)
+t_diff_C.convert(kelvin,T)
