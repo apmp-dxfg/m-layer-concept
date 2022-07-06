0c9dfd11415fa0c9410794a67a000d164d31e407
Blair Hall <blairdhall@gmail.com>
2022-07-06 14:40:45 +1200

    The set_conversion_function has been internalised and altered to allow conversion between interval-ratio pairs

--- m_layer/scales_for_aspect_register.py
+++ m_layer/scales_for_aspect_register.py
@@ -9,8 +9,60 @@ import json
 import math
 
 from m_layer import si_constants as si 
-from m_layer.conversion_register import _set_conversion_fn
+# from m_layer.conversion_register import _set_conversion_fn
 
+# ---------------------------------------------------------------------------
+def _set_conversion_fn(self,entry,_tbl, uid_pair):
+    """
+    Utility function to take one JSON entry for conversion between scales 
+    and enter it into a mapping, indexed by the pair of ML scale uids
+    
+    """
+
+    if uid_pair in _tbl:
+        raise RuntimeError(
+            "existing conversion entry: {}".format(uid_pair)
+        )
+        
+    # The M-Layer reference identifies the type of scale
+    _scales = self._context.scale_reg
+    src_type = _scales[ uid_pair[0] ]['scale_type']
+    dst_type = _scales[ uid_pair[1] ]['scale_type']
+
+    # if src_type != dst_type:
+        # raise RuntimeError(
+            # "scale types must be the same: {} and {}".format(
+            # src_type,dst_type)
+        # )
+                              
+    # Conversion function parameter values are in a sequence 
+    # they are stored as strings to allow fractions 
+    factors = tuple(  eval(x_i) for x_i in entry['factors'] )
+    
+    # Set the conversion function
+    if (src_type,dst_type) == ('ratio','ratio'):
+        _tbl[uid_pair] = lambda x: factors[0]*x 
+    elif (
+        (src_type,dst_type) == ('interval','interval') 
+    or  (src_type,dst_type) == ('ratio','interval')
+    or  (src_type,dst_type) == ('interval','ratio')
+    ):
+        # `factors[0]` is the scale divisions conversion factor 
+        # `factors[1]` is the offset
+        _tbl[uid_pair] = lambda x: factors[0]*x + factors[1]
+    elif (
+        (src_type,dst_type) == ('bounded','bounded')
+    ):
+        # `factors[0]` is the scale divisions conversion factor 
+        # `factors[1]` is the lower bound of the dst scale 
+        # `factors[2]` is the range of values in the dst scale
+        _tbl[uid_pair] = lambda x: \
+            (factors[0]*x - + factors[1]) % factors[2] + factors[1]
+    else:
+        raise RuntimeError(
+            "unrecognised case: {}".format((src_type,dst_type))
+        )
+ 
 # ---------------------------------------------------------------------------
 class ScalesForAspectRegister(object):
 
