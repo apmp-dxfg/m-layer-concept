c76493cee971251b517ce74453667ab872b32ce9
Blair Hall <blairdhall@gmail.com>
2022-09-07 13:28:52 +1200

    String formatting has been adjusted and the json method now included keyword arguments

--- m_layer/uid.py
+++ m_layer/uid.py
@@ -1,5 +1,6 @@
 import json
 from collections import defaultdict
+from fractions import Fraction
 
 from m_layer.stack import normal_form
 
@@ -7,6 +8,12 @@ __all__ = (
     'UID',
     'ComposedUID'
 )
+
+# ---------------------------------------------------------------------------
+# The uid key for a ScaleAspect is a tuple of UIDs, 
+# so str(k) calls the __repr__ UID method on the elements.
+#
+fmt_key = lambda k: "[{0[0]}, {0[1]}]".format(k)
 
 # ---------------------------------------------------------------------------
 # Encapsulate the format of M-layer UIDs
@@ -17,10 +24,10 @@ class UID(object):
     __slots__ = ('_m_layer_uuid',)
     
     """
-    M = UID( (
+    M = UID( [
         'ml_imp_pound_ratio', 
         188380796861507506602975683857494523991
-    ) )
+    ] )
     """
     
     def __init__(self,uid):
@@ -56,28 +63,18 @@ class UID(object):
         "The M-layer identifier"
         return self._m_layer_uuid
         
-    @property
-    def json(self):
+    def json(self,**kwargs):
         obj = dict(
             __type__ = "UID",
-            uid = str( list(
-                self.name,
-                self.uuid
-            ) ) 
+            uid = str( list(self._m_layer_uuid) ) 
         )
-        return json.dumps(obj)
+        return json.dumps(obj,**kwargs)
         
     def __str__(self):
-        return "({!r}, {})".format(
-            self.name,
-            self.uuid
-        )
+        return str(list(self._m_layer_uuid))
         
     def __repr__(self):
-        return "UID({!r}, {})".format(
-            self.name,
-            self.uuid
-        )
+        return "UID( {} )".format(list(self._m_layer_uuid))
   
 # ---------------------------------------------------------------------------
 class ComposedUID(object):
@@ -85,6 +82,7 @@ class ComposedUID(object):
     __slots__ = ('prefactor','factors')
 
     def __init__(self,stack):
+    
         pops = normal_form(stack)
 
         # The keys in  pop.factors are Python objects.
@@ -93,7 +91,7 @@ class ComposedUID(object):
         # The ComposedUID representation uses the M-layer UID as key 
         # and a forzenset of exponents as value. 
         
-        setter = lambda factors,i,v: factors[ UID(i.uid) ].add(v)
+        setter = lambda factors,i,v: factors[ i.uid ].add(v)
         
         factors = defaultdict(set)
         for k,v in pops.factors.items():
@@ -127,33 +125,61 @@ class ComposedUID(object):
  
     def __str__(self):
         factors = ", ".join(
-            "{} : {}".format(k,list(v) )
-                for k,v in self.factors.items()
-        )
-        return "{{ factors = {{ {} }}, prefactor = {} }}".format(
-            factors,
-            self.prefactor
-        )
-
+                "{} : {}".format(k if isinstance(k,UID) else fmt_key(k), list(v) 
+            ) for k,v in self.factors.items() 
+        ) 
+        
+        if self.prefactor != 1:
+            return "{{ {} ,prefactor={}) }}".format(
+                factors,
+                self.prefactor
+            )
+        else:
+            return "{{ {} }}".format(factors)
+            
     def __repr__(self):
         factors = ", ".join(
-            "{} : {}".format(k,list(v) )
-                for k,v in self.factors.items()
-        )
-        return "ComposedUID({{ {} }},prefactor={})".format(
-            factors,
-            self.prefactor
-        )
+                "{} : {}".format(k if isinstance(k,UID) else fmt_key(k), list(v) 
+            ) for k,v in self.factors.items() 
+        ) 
         
-    @property
-    def json(self):
-        factors = ", ".join(
-            "{} : {}".format(k,list(v) )
-                for k,v in self.factors.items()
-        )
-        obj = dict(
-            __type__ = "ComposedUID",
-            factors = factors,
-            prefactor = self.prefactor
-        )
-        return json.dumps(obj)
+        if self.prefactor != 1:
+            return "ComposedUID({{ {} }},prefactor={})".format(
+                factors,
+                self.prefactor
+            )
+        else:
+            return "ComposedUID({})".format(factors)
+        
+        
+    def json(self,**kwargs):
+        """
+        Return a JSON record for the ComposedUID
+        
+        Args:
+            kwargs: will be passed directly to json.dumps
+            
+        """
+        factors = {
+            str(k) if isinstance(k,UID) else fmt_key(k) : list(v) 
+                    for k,v in self.factors.items()
+        }
+
+        if self.prefactor != 1:
+            if isinstance(self.prefactor,Fraction):
+                prefactor = list( self.prefactor.as_integer_ratio() )
+            else:
+                prefactor = [ str(self.prefactor), "1" ]
+
+            obj = dict(
+                __type__ = "ComposedUID",
+                factors = factors,
+                prefactor = prefactor
+            )
+        else:
+            obj = dict(
+                __type__ = "ComposedUID",
+                factors = factors
+            )
+
+        return json.dumps(obj,**kwargs)
