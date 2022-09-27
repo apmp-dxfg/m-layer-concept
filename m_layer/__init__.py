835cc24738e1678e50e31fe1dd17e6a989e5c492
Blair Hall <blairdhall@gmail.com>
2022-09-27 15:13:41 +1300

    Bumped version

--- m_layer/__init__.py
+++ m_layer/__init__.py
@@ -4,7 +4,7 @@ from .api import *
 from m_layer.api import __all__
 
 #----------------------------------------------------------------------------
-version = "0.3.0.dev0"
+version = "0.4.0.dev0"
 copyright = """Copyright (c) 2022, Asia-Pacific Metrology Programme Focus Group on Digital Transformation in Metrology (APMP-DXFG)"""
 
 
